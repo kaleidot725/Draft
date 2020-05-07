836f0a37243c799ff8da5970357c0739ad101c0c
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-05-07 23:15:56 +0900

    Fix HomeDialogFragment Layout

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/homedialog/HomeDialogFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/homedialog/HomeDialogFragment.kt
@@ -8,8 +8,15 @@ import androidx.fragment.app.DialogFragment
 import jp.kaleidot725.emomemo.R
 
 class HomeDialogFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.DialogStyle);
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home_dialog, container, false)
