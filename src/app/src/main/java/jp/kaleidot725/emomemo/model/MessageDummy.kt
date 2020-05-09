a14b4c5c541f5b17ee2021856bbb71fab0f07de9
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-05-09 17:15:31 +0900

    Update Memo Layout

---
+++ src/app/src/main/java/jp/kaleidot725/emomemo/model/MessageDummy.kt
@@ -0,0 +1,14 @@
+package jp.kaleidot725.emomemo.model
+
+val MESSAGE_LIST = listOf(
+    Message(0, "Good Message"),
+    Message(1, "Good Message"),
+    Message(2, "Good Message"),
+    Message(3, "Good Message"),
+    Message(4, "Good Message"),
+    Message(5, "Good Message"),
+    Message(6, "Good Message"),
+    Message(7, "Good Message"),
+    Message(8, "Good Message"),
+    Message(9, "Good Message")
+)
--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/memo/MemoFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/memo/MemoFragment.kt
@@ -1,12 +1,16 @@
 package jp.kaleidot725.emomemo.ui.memo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import jp.kaleidot725.emomemo.R
+import jp.kaleidot725.emomemo.model.MESSAGE_LIST
+import jp.kaleidot725.emomemo.model.Message
+import jp.kaleidot725.emomemo.ui.core.MessageItemRecyclerViewController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MemoFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +18,23 @@ class MemoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_memo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val messageItemRecylerViewController = MessageItemRecyclerViewController(object :
+            MessageItemRecyclerViewController.SelectListener {
+            override fun onSelected(item: Message) {}
+        })
+
+        recycler_view.apply {
+            this.adapter = messageItemRecylerViewController.adapter
+            this.layoutManager = LinearLayoutManager(context).apply {
+                orientation = LinearLayoutManager.VERTICAL
+            }
+        }
+
+        messageItemRecylerViewController.setData(MESSAGE_LIST, false)
     }
 }
