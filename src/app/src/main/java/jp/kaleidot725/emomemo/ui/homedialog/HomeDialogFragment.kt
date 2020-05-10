ff107cdedf026968f735780eeaf3cec8dac6b0e3
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-05-10 15:34:55 +0900

    Add Exit HomeDialogFragment Logic

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/homedialog/HomeDialogFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/homedialog/HomeDialogFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import jp.kaleidot725.emomemo.R
+import kotlinx.android.synthetic.main.fragment_home_dialog.*
 
 class HomeDialogFragment : DialogFragment() {
+    private val navController: NavController get() = findNavController()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +24,17 @@ class HomeDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        complete_button.setOnClickListener {
+            navController.popBackStack()
+        }
+
+        cancel_button.setOnClickListener {
+            navController.popBackStack()
+        }
     }
 }
