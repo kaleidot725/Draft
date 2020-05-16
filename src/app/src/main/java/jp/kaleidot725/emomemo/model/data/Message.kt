588727c717171629d195b7dd7cfb70b76910caba
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-05-16 12:24:06 +0900

    Add MemoId to Message

--- src/app/src/main/java/jp/kaleidot725/emomemo/model/data/Message.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/model/data/Message.kt
@@ -1,3 +1,3 @@
 package jp.kaleidot725.emomemo.model.data
 
-data class Message(val time: Long, val value: String)
+data class Message(val memoId: String, val time: Long, val value: String)
--- src/app/src/main/java/jp/kaleidot725/emomemo/model/repository/MessageDummy.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/model/repository/MessageDummy.kt
@@ -3,14 +3,14 @@ package jp.kaleidot725.emomemo.model.repository
 import jp.kaleidot725.emomemo.model.data.Message
 
 val MESSAGE_LIST = listOf(
-    Message(0, "Good Message"),
-    Message(1, "Good Message"),
-    Message(2, "Good Message"),
-    Message(3, "Good Message"),
-    Message(4, "Good Message"),
-    Message(5, "Good Message"),
-    Message(6, "Good Message"),
-    Message(7, "Good Message"),
-    Message(8, "Good Message"),
-    Message(9, "Good Message")
+    Message("0", 0, "Good Message"),
+    Message("1", 1, "Good Message"),
+    Message("2", 2, "Good Message"),
+    Message("3", 3, "Good Message"),
+    Message("4", 4, "Good Message"),
+    Message("5", 5, "Good Message"),
+    Message("6", 6, "Good Message"),
+    Message("7", 7, "Good Message"),
+    Message("8", 8, "Good Message"),
+    Message("9", 9, "Good Message")
 )
