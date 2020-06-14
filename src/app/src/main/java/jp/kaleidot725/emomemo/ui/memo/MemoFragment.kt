79803b209a090da1440c02656ce2900bb969f276
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-06-14 23:12:39 +0900

    Add AudioRecognizerDialogFragment

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/memo/MemoFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/memo/MemoFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import jp.kaleidot725.emomemo.R
 import jp.kaleidot725.emomemo.databinding.FragmentMemoBinding
 import jp.kaleidot725.emomemo.extension.viewBinding
 import jp.kaleidot725.emomemo.ui.core.MessageItemRecyclerViewController
-import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.recycler_view
+import kotlinx.android.synthetic.main.fragment_memo.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MemoFragment : Fragment(R.layout.fragment_memo) {
@@ -18,6 +21,7 @@ class MemoFragment : Fragment(R.layout.fragment_memo) {
     private val binding: FragmentMemoBinding by viewBinding()
     private val args: MemoFragmentArgs by navArgs()
     private val messageItemRecyclerViewController = MessageItemRecyclerViewController()
+    private val navController: NavController get() = findNavController()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,12 +35,16 @@ class MemoFragment : Fragment(R.layout.fragment_memo) {
             }
         }
 
+        voice_button.setOnClickListener {
+            navController.navigate(R.id.action_memoFragment_to_audioRecognizerFragment)
+        }
+        
         viewModel.messageList.observe(viewLifecycleOwner, Observer {
             messageItemRecyclerViewController.setData(it, true)
         })
 
         viewModel.memoId = args.memoId.toInt()
-        
+
         viewModel.fetch()
     }
 }
