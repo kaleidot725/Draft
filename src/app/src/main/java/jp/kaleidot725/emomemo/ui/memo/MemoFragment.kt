6be28caf800805e96af9c1a6ba46815864c40d3f
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-06-15 21:52:06 +0900

    Add Audio Record Permissions

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/memo/MemoFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/memo/MemoFragment.kt
@@ -1,7 +1,9 @@
 package jp.kaleidot725.emomemo.ui.memo
 
+import android.Manifest.permission.RECORD_AUDIO
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
@@ -15,7 +17,14 @@ import jp.kaleidot725.emomemo.ui.core.MessageItemRecyclerViewController
 import kotlinx.android.synthetic.main.fragment_home.recycler_view
 import kotlinx.android.synthetic.main.fragment_memo.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnNeverAskAgain
+import permissions.dispatcher.OnPermissionDenied
+import permissions.dispatcher.OnShowRationale
+import permissions.dispatcher.PermissionRequest
+import permissions.dispatcher.RuntimePermissions
 
+@RuntimePermissions
 class MemoFragment : Fragment(R.layout.fragment_memo) {
     private val viewModel: MemoViewModel by viewModel()
     private val binding: FragmentMemoBinding by viewBinding()
@@ -25,26 +34,51 @@ class MemoFragment : Fragment(R.layout.fragment_memo) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupLayout()
+        setupViewModel()
+    }
 
-        binding.viewModel = viewModel
-
-        recycler_view.apply {
-            this.adapter = messageItemRecyclerViewController.adapter
-            this.layoutManager = LinearLayoutManager(context).apply {
-                orientation = LinearLayoutManager.VERTICAL
-            }
+    private fun setupLayout() {
+        recycler_view.adapter = messageItemRecyclerViewController.adapter
+        recycler_view.layoutManager = LinearLayoutManager(context).apply {
+            orientation = LinearLayoutManager.VERTICAL
         }
-
         voice_button.setOnClickListener {
-            navController.navigate(R.id.action_memoFragment_to_audioRecognizerFragment)
+            showRecordAudioWithPermissionCheck()
         }
+    }
 
+    private fun setupViewModel() {
+        binding.viewModel = viewModel
         viewModel.messageList.observe(viewLifecycleOwner, Observer {
             messageItemRecyclerViewController.setData(it, true)
         })
-
         viewModel.memoId = args.memoId.toInt()
-
         viewModel.fetch()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    @NeedsPermission(RECORD_AUDIO)
+    fun showRecordAudio() {
+        navController.navigate(R.id.action_memoFragment_to_audioRecognizerFragment)
+    }
+
+    @OnShowRationale(RECORD_AUDIO)
+    fun showRationaleForRecordAudio(request: PermissionRequest) {
+        request.proceed()
+    }
+
+    @OnPermissionDenied(RECORD_AUDIO)
+    fun onRecordAudioDenied() {
+        Toast.makeText(context, R.string.memo_fragment_audio_permission_denied, Toast.LENGTH_SHORT).show()
+    }
+
+    @OnNeverAskAgain(RECORD_AUDIO)
+    fun onRecordAudioNeverAskAgain() {
+        Toast.makeText(context, R.string.memo_fragment_audio_permission_never_ask_again, Toast.LENGTH_SHORT).show()
     }
 }
