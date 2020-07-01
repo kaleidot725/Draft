9af928096225a6ee6a8b76544555e3b4eb177f5f
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-01 23:09:48 +0900

    Remove Test Data

--- src/app/src/main/java/jp/kaleidot725/emomemo/model/db/repository/MemoRepository.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/model/db/repository/MemoRepository.kt
@@ -3,17 +3,6 @@ package jp.kaleidot725.emomemo.model.db.repository
 import jp.kaleidot725.emomemo.model.db.dao.MemoDao
 import jp.kaleidot725.emomemo.model.db.entity.MemoEntity
 
-val DUMMY_MEMO_LIST = listOf(
-    MemoEntity(0, "英語", "発音の仕方"),
-    MemoEntity(1, "英語", "発音の応用"),
-    MemoEntity(2, "数学", "微分・積分の仕方"),
-    MemoEntity(3, "数学", "二次方程式についてのまとめ"),
-    MemoEntity(4, "ただのメモ", "今日の買い物"),
-    MemoEntity(5, "ただのメモ", "明日の買い物"),
-    MemoEntity(6, "ごみばこ", "単語を覚えるためのメモ"),
-    MemoEntity(7, "ごみばこ", "ラーメンをうまくつくるためのメモ")
-)
-
 class MemoRepository(private val dao: MemoDao) {
     fun insert(memo: MemoEntity) {
         dao.insert(memo)
