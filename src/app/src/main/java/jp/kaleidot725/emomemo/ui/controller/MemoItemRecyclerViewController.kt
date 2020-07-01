1d9b6eb8fef79035417680654f3249a9aa0d7073
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-01 23:23:21 +0900

    Add time and message setting when memo hasn't messages

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/controller/MemoItemRecyclerViewController.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/controller/MemoItemRecyclerViewController.kt
@@ -1,11 +1,14 @@
 package jp.kaleidot725.emomemo.ui.controller
 
+import android.content.Context
 import android.view.View
 import com.airbnb.epoxy.TypedEpoxyController
+import jp.kaleidot725.emomemo.R
 import jp.kaleidot725.emomemo.memoItemContainer
 import jp.kaleidot725.emomemo.model.ddd.domain.Memo
 
 class MemoItemRecyclerViewController(
+    private val context: Context,
     private val selectListener: SelectListener? = null
 ) : TypedEpoxyController<List<Memo>>() {
 
@@ -14,6 +17,8 @@ class MemoItemRecyclerViewController(
             memoItemContainer {
                 id(item.id)
                 title(item.title)
+                latestTime(context.getString(R.string.memo_no_message_time))
+                latestMessage(context.getString(R.string.memo_no_message_text))
                 onClickListener(View.OnClickListener { selectListener?.onSelected(item) })
             }
         }
