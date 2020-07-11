0170e3407adfa8bd69f67cbe0c9679bb24898a52
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-11 16:08:35 +0900

    Fix Flipper Setup

--- src/app/src/main/java/jp/kaleidot725/emomemo/EmomemoApp.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/EmomemoApp.kt
@@ -3,6 +3,7 @@ package jp.kaleidot725.emomemo
 import android.app.Application
 import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
 import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
 import com.facebook.soloader.SoLoader
@@ -19,7 +20,7 @@ class EmomemoApp : Application() {
         setupFlipper()
     }
 
-    fun setupDI() {
+    private fun setupDI() {
         startKoin {
             androidLogger()
             androidContext(applicationContext)
@@ -27,11 +28,12 @@ class EmomemoApp : Application() {
         }
     }
 
-    fun setupFlipper() {
+    private fun setupFlipper() {
         SoLoader.init(this, false)
         if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
             val client = AndroidFlipperClient.getInstance(this)
             client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(DatabasesFlipperPlugin(this))
             client.start()
         }
     }
