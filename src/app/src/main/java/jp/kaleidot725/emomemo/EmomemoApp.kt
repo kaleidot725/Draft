c103e3a32a88f77beeb5a2767da6da743821915e
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-11 16:44:17 +0900

    Fix Lint Error

--- src/app/src/main/java/jp/kaleidot725/emomemo/EmomemoApp.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/EmomemoApp.kt
@@ -12,7 +12,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-
 class EmomemoApp : Application() {
     override fun onCreate() {
         super.onCreate()
