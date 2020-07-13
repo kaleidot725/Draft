c91630d14112b6c005585daac127e9f61bcb9add
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-13 21:01:43 +0900

    Add Navigation Drawer

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/MainActivity.kt
@@ -7,17 +7,22 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import jp.kaleidot725.emomemo.R
+import kotlinx.android.synthetic.main.activity_main.action_bar
+import kotlinx.android.synthetic.main.activity_main.drawer_layout
+import kotlinx.android.synthetic.main.activity_main.nav_view
 
 class MainActivity : AppCompatActivity() {
     private val navController: NavController get() = findNavController(R.id.nav_host_fragment)
     private val appBarConfiguration: AppBarConfiguration by lazy {
-        AppBarConfiguration.Builder(setOf(R.id.homeFragment)).build()
+        AppBarConfiguration.Builder(setOf(R.id.homeFragment)).setDrawerLayout(drawer_layout).build()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(action_bar)
         setupActionBarWithNavController(this, navController, appBarConfiguration)
 
         navController.addOnDestinationChangedListener { controller, _, _ ->
@@ -26,6 +31,8 @@ class MainActivity : AppCompatActivity() {
                 else -> supportActionBar?.show()
             }
         }
+
+        nav_view.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
