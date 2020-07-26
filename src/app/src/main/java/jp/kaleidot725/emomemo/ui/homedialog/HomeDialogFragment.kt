c39d371126b7cb29bb217bea498a14639a84c3f5
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-26 16:38:05 +0900

    Refactor HomeDialogFragment and HomeDialogViewModel

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/homedialog/HomeDialogFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/homedialog/HomeDialogFragment.kt
@@ -9,12 +9,15 @@ import androidx.navigation.fragment.findNavController
 import jp.kaleidot725.emomemo.R
 import jp.kaleidot725.emomemo.databinding.FragmentHomeDialogBinding
 import jp.kaleidot725.emomemo.extension.viewBinding
+import jp.kaleidot725.emomemo.ui.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeDialogFragment : DialogFragment(R.layout.fragment_home_dialog) {
     private val navController: NavController get() = findNavController()
     private val binding: FragmentHomeDialogBinding by viewBinding()
-    private val viewModel: HomeDialogViewModel by viewModel()
+    private val mainViewModel: MainViewModel by sharedViewModel()
+    private val homeDialogViewModel: HomeDialogViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +26,20 @@ class HomeDialogFragment : DialogFragment(R.layout.fragment_home_dialog) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewModel = this.viewModel
-        viewModel.event.observe(viewLifecycleOwner, Observer {
-            when (it) {
-                HomeDialogViewModel.NavEvent.SUCCESS -> navController.popBackStack()
-                HomeDialogViewModel.NavEvent.CANCEL -> navController.popBackStack()
+        binding.mainViewModel = mainViewModel
+        binding.homeDialogViewModel = homeDialogViewModel
+        homeDialogViewModel.event.observe(viewLifecycleOwner, Observer { event ->
+            when (event) {
+                HomeDialogViewModel.NavEvent.SUCCESS -> {
+                    mainViewModel.createMemo(homeDialogViewModel.title.value ?: "")
+                    navController.popBackStack()
+                }
+                HomeDialogViewModel.NavEvent.CANCEL -> {
+                    navController.popBackStack()
+                }
+                else -> {
+                    navController.popBackStack()
+                }
             }
         })
     }
--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/homedialog/HomeDialogViewModel.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/homedialog/HomeDialogViewModel.kt
@@ -3,14 +3,9 @@ package jp.kaleidot725.emomemo.ui.homedialog
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.hadilq.liveevent.LiveEvent
 import jp.kaleidot725.emomemo.model.AppStatus
-import jp.kaleidot725.emomemo.model.db.entity.MemoEntity
 import jp.kaleidot725.emomemo.model.db.repository.MemoRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class HomeDialogViewModel(
     private val appStatus: AppStatus,
@@ -21,15 +16,8 @@ class HomeDialogViewModel(
     val title: MutableLiveData<String> = MutableLiveData()
 
     fun success() {
-        if (title.value == null) {
-            return
-        }
-
-        viewModelScope.launch(Dispatchers.IO) {
-            memoRepository.insert(MemoEntity.create(appStatus.notebookId, title.value ?: ""))
-            withContext(Dispatchers.Main) {
-                _event.postValue(NavEvent.SUCCESS)
-            }
+        if (title.value?.isNotEmpty() == true) {
+            _event.postValue(NavEvent.SUCCESS)
         }
     }
 
