59994733a952feb48e90620ef5f77ae0762f0dd0
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-07-28 02:01:07 +0900

    Fix Spinner Layout

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/notebook/DeleteNotebookDialogFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/notebook/DeleteNotebookDialogFragment.kt
@@ -30,23 +30,12 @@ class DeleteNotebookDialogFragment : DialogFragment(R.layout.fragment_delete_not
         super.onViewCreated(view, savedInstanceState)
         binding.mainViewModel = mainViewModel
         binding.deleteNotebookViewModel = deleteNotebookViewModel
+        binding.notebookSpinner.adapter = createSpinnerAdapter(null)
 
-        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        binding.notebookSpinner.apply {
-            this.adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item).apply {
-                this.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            }
-
-            this.onItemSelectedListener = object : AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
-                override fun onNothingSelected(parent: AdapterView<*>?) {}
-                override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {}
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    deleteNotebookViewModel.onNotebookSelected(position)
-                }
-            }
-        }
+        deleteNotebookViewModel.notebookTitles.observe(viewLifecycleOwner, Observer {
+            binding.notebookSpinner.adapter = createSpinnerAdapter(it)
+            binding.notebookSpinner.onItemSelectedListener = createSpinnerOnItemSelectListener()
+        })
 
         deleteNotebookViewModel.event.observe(viewLifecycleOwner, Observer { event ->
             when (event) {
@@ -63,5 +52,26 @@ class DeleteNotebookDialogFragment : DialogFragment(R.layout.fragment_delete_not
             }
         })
         deleteNotebookViewModel.fetch()
+    }
+
+    private fun createSpinnerAdapter(data: List<String>?): ArrayAdapter<String> {
+        return if (data != null) {
+            ArrayAdapter<String>(requireContext(), R.layout.spinner_item, R.id.item_text_view, data).apply {
+                setDropDownViewResource(R.layout.spinner_drop_down_item)
+            }
+        } else {
+            ArrayAdapter<String>(requireContext(), R.layout.spinner_item, R.id.item_text_view).apply {
+                setDropDownViewResource(R.layout.spinner_drop_down_item)
+            }
+        }
+    }
+
+    private fun createSpinnerOnItemSelectListener(): AdapterView.OnItemSelectedListener {
+        return object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                deleteNotebookViewModel.onNotebookSelected(position)
+            }
+        }
     }
 }
