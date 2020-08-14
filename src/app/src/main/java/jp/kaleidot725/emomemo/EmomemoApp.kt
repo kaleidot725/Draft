c4342735ca7fd55aaeb876d9239397aee2d62128
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-08-15 00:50:21 +0900

    Delete Flipper

--- src/app/src/main/java/jp/kaleidot725/emomemo/EmomemoApp.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/EmomemoApp.kt
@@ -1,12 +1,6 @@
 package jp.kaleidot725.emomemo
 
 import android.app.Application
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
 import jp.kaleidot725.emomemo.model.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -16,7 +10,6 @@ class EmomemoApp : Application() {
     override fun onCreate() {
         super.onCreate()
         setupDI()
-        setupFlipper()
     }
 
     private fun setupDI() {
@@ -24,16 +17,6 @@ class EmomemoApp : Application() {
             androidLogger()
             androidContext(applicationContext)
             modules(appModule)
-        }
-    }
-
-    private fun setupFlipper() {
-        SoLoader.init(this, false)
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-            val client = AndroidFlipperClient.getInstance(this)
-            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
-            client.addPlugin(DatabasesFlipperPlugin(this))
-            client.start()
         }
     }
 }
