4a1572e471f28da330bd2c95b27385742e35f1d4
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-08-23 22:54:44 +0900

    feat: modify package

--- src/app/src/main/java/jp/kaleidot725/emomemo/EmomemoApp.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/EmomemoApp.kt
@@ -1,7 +1,6 @@
 package jp.kaleidot725.emomemo
 
 import android.app.Application
-import jp.kaleidot725.emomemo.model.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
