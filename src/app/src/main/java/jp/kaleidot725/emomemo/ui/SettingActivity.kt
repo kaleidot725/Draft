05793029d82501b8426e762538e789a0aa584c6e
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-13 15:51:52 +0900

    feat: impl setting activity and fragment

---
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/SettingActivity.kt
@@ -0,0 +1,8 @@
+package jp.kaleidot725.emomemo.ui
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.kaleidot725.emomemo.R
+
+class SettingActivity : AppCompatActivity(R.layout.activity_setting)
--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/SettingFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/SettingFragment.kt
@@ -3,11 +3,11 @@ package jp.kaleidot725.emomemo.ui.setting
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import jp.kaleidot725.emomemo.R
 
-class SettingFragment : Fragment(R.layout.fragment_setting) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        requireActivity().title = getString(R.string.setting_fragment_title)
+class SettingFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 }
