1518e0997337ff748f78a74b549b81dd9b5c284f
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-14 22:56:55 +0900

    feat: impl setting menu navigation

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/SettingFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/SettingFragment.kt
@@ -1,11 +1,23 @@
 package jp.kaleidot725.emomemo.ui.setting
 
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import jp.kaleidot725.emomemo.R
 
 class SettingFragment : PreferenceFragmentCompat() {
+    private val navController get() = findNavController()
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+        findPreference<Preference>(getString(R.string.privacy_policy))?.setOnPreferenceClickListener {
+            navController.navigate(R.id.action_settingFragment_to_privacyPolicyFragment)
+            true
+        }
+        findPreference<Preference>(getString(R.string.license))?.setOnPreferenceClickListener {
+            navController.navigate(R.id.action_settingFragment_to_licenseFragment)
+            true
+        }
     }
 }
