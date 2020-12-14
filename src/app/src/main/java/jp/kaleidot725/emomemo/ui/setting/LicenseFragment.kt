2f817ae29cb7bba7119fffa1e5dcd9fcf24586b0
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-14 22:31:06 +0900

    feat: add LicenseFragment and PrivacyPolicyFragment

---
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/LicenseFragment.kt
@@ -0,0 +1,6 @@
+package jp.kaleidot725.emomemo.ui.setting
+
+import androidx.fragment.app.Fragment
+import jp.kaleidot725.emomemo.R
+
+class LicenseFragment : Fragment(R.layout.fragment_license)
---
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/PrivacyPolicyFragment.kt
@@ -0,0 +1,6 @@
+package jp.kaleidot725.emomemo.ui.setting
+
+import androidx.fragment.app.Fragment
+import jp.kaleidot725.emomemo.R
+
+class PrivacyPolicyFragment : Fragment(R.layout.fragment_privacy_policy)
