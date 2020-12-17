0a89cbe08e6a8b710b6aed424dde38113e0b282d
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-17 23:10:54 +0900

    style: fix lint error

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/SettingFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/SettingFragment.kt
@@ -6,7 +6,6 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import jp.kaleidot725.emomemo.R
 
-
 class SettingFragment : PreferenceFragmentCompat() {
     private val navController get() = findNavController()
 
