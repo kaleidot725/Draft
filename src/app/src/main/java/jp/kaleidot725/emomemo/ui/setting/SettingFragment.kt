e7a121b5fbe6705a3e3f8426eb0ec1910b30f1f6
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-17 22:40:46 +0900

    feat: delete privacy policy fragment

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/SettingFragment.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/setting/SettingFragment.kt
@@ -11,11 +11,7 @@ class SettingFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        findPreference<Preference>(getString(R.string.privacy_policy))?.setOnPreferenceClickListener {
-            navController.navigate(R.id.action_settingFragment_to_privacyPolicyFragment)
-            true
-        }
-        findPreference<Preference>(getString(R.string.license))?.setOnPreferenceClickListener {
+        findPreference<Preference>(getString(R.string.preference_license))?.setOnPreferenceClickListener {
             navController.navigate(R.id.action_settingFragment_to_licenseFragment)
             true
         }
