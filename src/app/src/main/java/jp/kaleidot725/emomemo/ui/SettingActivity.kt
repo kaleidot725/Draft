7c7be89b0e708b801a8af483b9531999d4c924e5
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-31 16:06:38 +0900

    feat: update setting activity theme

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/SettingActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/SettingActivity.kt
@@ -2,19 +2,33 @@ package jp.kaleidot725.emomemo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import jp.kaleidot725.emomemo.R
+import jp.kaleidot725.emomemo.databinding.ActivitySettingBinding
 
-class SettingActivity : AppCompatActivity(R.layout.activity_setting) {
+class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
     private val navController get() = findNavController(this, R.id.nav_host_fragment)
     private val appBarConfiguration get() = AppBarConfiguration(navController.graph)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupDataBinding()
+        setupNavController()
+    }
+
+    private fun setupDataBinding() {
+        binding = DataBindingUtil.setContentView<ActivitySettingBinding>(this, R.layout.activity_setting).also { binding ->
+            setSupportActionBar(binding.actionBar)
+        }
+    }
+
+    private fun setupNavController() {
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
