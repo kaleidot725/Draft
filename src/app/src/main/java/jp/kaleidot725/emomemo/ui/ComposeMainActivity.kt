a12dfd31150a90aea73c0fd24ade1bb2ce12bc80
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-03 01:31:52 +0900

    feat: add compose implementations

--- src/app/src/main/java/jp/kaleidot725/emomemo/ui/ComposeMainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/ComposeMainActivity.kt
@@ -2,9 +2,21 @@ package jp.kaleidot725.emomemo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 class ComposeMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent { MainApp() }
+    }
+}
+
+@Composable
+fun MainApp() {
+    MaterialTheme {
+        Text(text = "TEST TEST TEST")
     }
 }
