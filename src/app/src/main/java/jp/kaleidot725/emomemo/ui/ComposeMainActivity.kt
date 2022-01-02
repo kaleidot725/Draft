432465014d39f121bbc7c76b6a7021161d8f9e41
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-03 00:52:53 +0900

    feat: add ComposeMainActivity.kt

---
+++ src/app/src/main/java/jp/kaleidot725/emomemo/ui/ComposeMainActivity.kt
@@ -0,0 +1,10 @@
+package jp.kaleidot725.emomemo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComposeMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
