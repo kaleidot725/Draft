77e223de1dc433ba9e8b37473f76d4571cf5c02d
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-06-08 23:02:00 +0900

    Fix editor bug

--- src/app/src/main/java/jp/kaleidot725/emomemo/view/pages/memo/detail/MemoDetailPage.kt
+++ src/app/src/main/java/jp/kaleidot725/emomemo/view/pages/memo/detail/MemoDetailPage.kt
@@ -1,26 +1,33 @@
 package jp.kaleidot725.emomemo.view.pages.memo.detail
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.insets.navigationBarsWithImePadding
 import com.google.accompanist.insets.systemBarsPadding
-import jp.kaleidot725.emomemo.view.atoms.BasicTextFields
 import jp.kaleidot725.emomemo.view.organisms.topbar.MemoTopBar
 import kotlinx.coroutines.flow.collectLatest
 
@@ -56,18 +63,26 @@ fun MemoDetailPage(viewModel: MemoDetailViewModel, onBack: () -> Unit, onNavigat
             }
         },
         content = { contentPadding ->
-            Column(
+            var value by remember { mutableStateOf(uiState.memoEntity?.content ?: "") }
+            val focusRequester = remember { FocusRequester() }
+            Box(
                 modifier = Modifier
+                    .padding(4.dp)
                     .padding(contentPadding)
-                    .padding(8.dp)
                     .fillMaxHeight()
                     .verticalScroll(rememberScrollState())
-                    .navigationBarsWithImePadding(),
+                    .clickable(interactionSource = MutableInteractionSource(), indication = null) { focusRequester.requestFocus() }
             ) {
-                BasicTextFields.BodyLarge(
-                    text = uiState.memoEntity?.content ?: "",
-                    onValueChange = { viewModel.updateContent(it) },
-                    singleLine = false
+                BasicTextField(
+                    value = value,
+                    onValueChange = {
+                        value = it
+                        viewModel.updateContent(it)
+                    },
+                    modifier = Modifier
+                        .focusRequester(focusRequester)
+                        .fillMaxSize(),
+                    textStyle = MaterialTheme.typography.bodyLarge
                 )
             }
         },
